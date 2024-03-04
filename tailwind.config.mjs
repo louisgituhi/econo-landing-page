ec28f8d704394ec034e6d9f400e23716e672f05d
louisgituhi <louisgituhi@gmail.com>
2024-03-05 01:01:28 +0300

    initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				"manrope": ["Manrope", "sans-serif"]
+			},
+			animation: {
+				'infinite-scroll': 'infinite-scroll 25s linear infinite',
+			},
+			keyframes: {
+				'infinite-scroll': {
+					from: { transform: 'translateX(0)' },
+					to: { transform: 'translateX(-100%)' },
+				}
+			}     
+		},
+	},
+	plugins: [],
+}
